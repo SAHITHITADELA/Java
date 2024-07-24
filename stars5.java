fce86e41eeeaa3b4f2d1cdc65e8f3ce6615b4293
SAHITHITADELA <159775457+SAHITHITADELA@users.noreply.github.com>
2024-07-24 11:05:48 +0530

    stars5.java

---
+++ stars5.java
@@ -0,0 +1,11 @@
+public class sta{
+	public static void main(String[] args){
+		int i,j;
+		for(i=1;i<=3;i++){
+			for (j=0;j<i;j++){
+				System.out.print(i+j);
+				}
+			System.out.println();
+	}
+}
+}
